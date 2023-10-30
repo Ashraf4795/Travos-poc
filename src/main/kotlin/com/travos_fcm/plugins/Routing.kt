3a3e0a1532c64978055d4bafbf78a7bfa4d6cef9
Ashraf4795 <ashraf.mohamediti4728@gmail.com>
2023-10-31 01:03:11 +0200

    refine code

--- src/main/kotlin/com/travos_fcm/plugins/Routing.kt
+++ src/main/kotlin/com/travos_fcm/plugins/Routing.kt
@@ -1,15 +1,9 @@
 package com.travos_fcm.plugins
 
-import com.google.auth.oauth2.GoogleCredentials
-import com.google.firebase.FirebaseApp
-import com.google.firebase.FirebaseOptions
-import com.google.firebase.messaging.FirebaseMessaging
-import com.google.firebase.messaging.Message
 import com.travos_fcm.service.fcm.sendNotificationToDevice
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import java.io.FileInputStream
 
 
 fun Application.configureRouting() {
